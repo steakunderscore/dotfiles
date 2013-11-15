6f9e0d4a0ac5d6be2664593bf6b6fbb383d6954c
Henry Jenkins <henry@henryjenkins.name>
2013-11-15 21:29:15 +1300

    Added my own aliases removing un-used ones

---
+++ shell/aliases/aliases.sh
@@ -0,0 +1,8 @@
+alias history="fc -l 1"
+alias internet\?="ping 8.8.8.8"
+alias b="bundle"
+alias serve="python -m SimpleHTTPServer"
+alias ip='curl curlmyip.com'
+
+# copy with a progress bar.
+alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"

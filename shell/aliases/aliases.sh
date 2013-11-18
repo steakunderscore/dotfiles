e5c889f53370fa2c73b3d235a7daaf304ae330de
Henry Jenkins <henry@henryjenkins.name>
2013-11-19 10:53:24 +1300

    Added popd alias

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -3,6 +3,7 @@ alias internet\?="ping 8.8.8.8"
 alias b="bundle"
 alias serve="python -m SimpleHTTPServer"
 alias ip='curl curlmyip.com'
+alias p='popd'
 
 # copy with a progress bar.
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"

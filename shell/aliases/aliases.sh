4aca5ead3253d1f0a34ed6fc40f7095f8d479362
Henry Jenkins <henry@henryjenkins.name>
2014-12-23 23:21:10 +1300

    More fixing up

--- shell/aliases/aliases.sh
+++
@@ -1,9 +0,0 @@
-alias history="fc -l 1"
-alias internet\?="ping 8.8.8.8"
-alias b="bundle"
-alias serve="python -m SimpleHTTPServer"
-alias ip='curl curlmyip.com'
-alias p='popd'
-
-# copy with a progress bar.
-alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"

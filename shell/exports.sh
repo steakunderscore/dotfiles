83a408903d016c06e865c2528ecccd683d3d3f63
Henry Jenkins <henry@henryjenkins.name>
2014-12-23 23:21:10 +1300

    More fixing up

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,7 +2,7 @@ export DOTFILES=~/.dotfiles                 # here
 export ARCHFLAGS='-arch x86_64'             # archflags
 export GREP_OPTIONS='--color=auto'          # double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
-export GH_LOGIN=steakunderscore             # used by git-go
+export GH_LOGIN="$(git config github.username)" # used by git-go
 
 path_add() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
@@ -13,6 +13,13 @@ path_add() {
 path_add "/usr/local/sbin"
 path_add "/usr/local/share/npm/bin"
 
+if type go >/dev/null 2>&1; then
+  export GOPATH="$(realpath `which go` | sed "s/\/libexec\/bin\/go//")"
+  path_add "$GOPATH/bin"
+fi
+
+export PROJECTS=~/Dev/git
+
 # used by `dvcs` alias
-export GH_BASE=~/workspace/git
+export GH_BASE="$PROJECTS"
 export GH_USER="$GH_LOGIN"

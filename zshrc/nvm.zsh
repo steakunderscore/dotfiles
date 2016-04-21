38a03b2862dc3be2deb56fa8fd3d52b2338ccbf9
Henry Jenkins <henry@henryjenkins.name>
2016-04-21 16:49:25 -0400

    Fix nvm loading, rewrite github-url script

--- zshrc/nvm.zsh
+++ zshrc/nvm.zsh
@@ -1,5 +1,5 @@
-if [[ -a ~/.nvm ]]
+export NVM_DIR=$(readlink "$HOME/.nvm")
+if [[ -a $NVM_DIR ]]
 then
-  export NVM_DIR="/home/henry/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 fi

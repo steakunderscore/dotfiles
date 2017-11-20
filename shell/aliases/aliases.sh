ed05b7a8a1ac17bf57ca05950a96e4d322648237
Henry Jenkins <henry@henryjenkins.name>
2017-11-20 10:56:47 -0500

    Fix some aliases

---
+++ shell/aliases/aliases.sh
@@ -0,0 +1,34 @@
+alias ls="ls -F"
+alias ll="ls -lGFh"
+alias history="fc -l 1"
+alias internet\?="ping 8.8.8.8"
+alias b="bundle check || bundle"
+alias dev="cd $HOME/Dev"
+alias serve="python -m SimpleHTTPServer"
+
+# cat with highlight
+alias cah="pygmentize -g"
+
+# highest rated results first
+alias z="_z -r 2>&1"
+
+# copy with a progress bar.
+alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+
+d() {
+  if [[ -n "$1" ]]; then
+    cd "+$1"
+  else
+    dirs -v
+  fi
+}
+
+p() {
+  cd "$PROJECTS/$(find "$PROJECTS" -type d -maxdepth 3 -name .git | sed 's#/\.git$##' | sed "s#^$PROJECTS/##" | selecta)"
+}
+
+# source dotfiles
+reload() {
+  source $HOME/.zshrc &&
+  echo "Your dot files are now \033[1;32msourced\033[0m."
+}

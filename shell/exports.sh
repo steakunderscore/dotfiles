61862b2db01045edeeb5f682d8a0a7122c7bb1b5
Henry Jenkins <henry@henryjenkins.name>
2018-03-30 12:51:07 +0100

    Fix go path

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,5 @@
 export DOTFILES=~/.dotfiles                     # here
 export ARCHFLAGS='-arch x86_64'                 # archflags
-export PATH=$HOME/bin:/usr/local/bin:$PATH      # path settings
 export GH_LOGIN="$(git config github.username)" # used by git-go
 export GITHUB_URL="$(git config github.url)"    # used by gist
 
@@ -10,11 +9,13 @@ path_add() {
   fi
 }
 
+path_add "$HOME/bin"
+path_add "/usr/local/bin"
 path_add "/usr/local/sbin"
 path_add "/usr/local/share/npm/bin"
 
 if type go >/dev/null 2>&1; then
-  export GOPATH="$(realpath `which go` | sed "s/\/libexec\/bin\/go//")"
+  export GOPATH=$HOME/go
   path_add "$GOPATH/bin"
 fi
 

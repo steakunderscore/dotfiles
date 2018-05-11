db028f34b0c0f10e48211290dd78bef1d9aa660f
Henry Jenkins <henry@henryjenkins.name>
2018-05-11 11:21:48 +0100

    Fix docs

--- zshrc/oracle_instantclient.zsh
+++ zshrc/oracle_instantclient.zsh
@@ -1,4 +1,4 @@
-# chruby auto-switching
+# http://www.oracle.com/technetwork/database/database-technologies/instant-client/overview/index.html
 if [[ -d /opt/oracle/instantclient_12_2 ]]
 then
   export LD_LIBRARY_PATH=/opt/oracle/instantclient_12_2

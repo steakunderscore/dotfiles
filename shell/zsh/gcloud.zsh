8839a3f7b5b6b86b4986068ca603dca1ba1bdb82
Henry Jenkins <henry@henryjenkins.name>
2019-01-22 10:12:10 +0000

    Support google cloud SDK on Ubuntu

--- shell/zsh/gcloud.zsh
+++ shell/zsh/gcloud.zsh
@@ -1,5 +1,7 @@
-export GOOGLE_CLOUD_SDK_HOME="${GOOGLE_CLOUD_SDK_HOME:/opt/google-cloud-sdk}"
-if [[ -a $GOOGLE_CLOUD_SDK_HOME ]]
+[ -d /opt/google-cloud-sdk ] && export GOOGLE_CLOUD_SDK_HOME="/opt/google-cloud-sdk"
+[ -d /usr/share/google-cloud-sdk ] && export GOOGLE_CLOUD_SDK_HOME="/usr/share/google-cloud-sdk"
+
+if [[ -d $GOOGLE_CLOUD_SDK_HOME ]]
 then
   [ -f ${GOOGLE_CLOUD_SDK_HOME}/completion.zsh.inc ] && . "${GOOGLE_CLOUD_SDK_HOME}/completion.zsh.inc"
 fi

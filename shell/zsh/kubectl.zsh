83898383a03b8ebe2e546ff6ee47e63f8b8118d7
Henry Jenkins <henry@henryjenkins.name>
2019-01-26 13:26:45 +0000

    More conditions on loading kubectl configs

--- shell/zsh/kubectl.zsh
+++ shell/zsh/kubectl.zsh
@@ -2,7 +2,13 @@ if [ $commands[kubectl] ]; then
   source <(kubectl completion $(basename ${SHELL}))
 fi
 
-export KUBECONFIG=${HOME}/.kube/config
-for file in ${HOME}/.kube/configs/*.yaml; do
-  export KUBECONFIG=$KUBECONFIG:$file
-done
+# If initial kube config exists
+if [ -f ${HOME}/.kube/config ]; then
+  export KUBECONFIG=${HOME}/.kube/config
+  # Check for more configs
+  if [ -d ${HOME}/.kube/configs/ ]; then
+    for file in ${HOME}/.kube/configs/*.yaml; do
+      export KUBECONFIG=$KUBECONFIG:$file
+    done
+  fi
+fi

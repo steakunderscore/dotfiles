d939dd6d88f7822fbce481e9e5c55611c2b982d0
Henry Jenkins <henry@henryjenkins.name>
2022-07-25 09:49:37 +0100

    Only load gems bin folders if gem folder exists
    To prevent find complaining about the missing folder

--- shell/sh/rubygems.sh
+++ shell/sh/rubygems.sh
@@ -1,4 +1,9 @@
-for p in $(find $HOME/.gem/ruby/ -maxdepth 1 -mindepth 1 -type d | sort --reverse)
-do
-  path_add "${p}/bin"
-done
+GEM_HOME="${HOME}/.gem"
+
+if [ -d "${GEM_HOME}" ]
+then
+  for p in $(find "${GEM_HOME}/ruby/" -maxdepth 1 -mindepth 1 -type d | sort --reverse)
+  do
+    path_add "${p}/bin"
+  done
+fi

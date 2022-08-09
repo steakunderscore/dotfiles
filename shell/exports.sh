3ed015207b94e067162202a1d0a55bf2e8eed69b
Henry Jenkins <henry@henryjenkins.name>
2022-08-09 20:49:00 +0100

    Update my github clone scripts.
    Mostly fixing the setting of the upstream and user fork remotes.

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,6 @@
 export DOTFILES=~/.dotfiles                     # here
 export ARCHFLAGS='-arch x86_64'                 # archflags
-export GH_LOGIN="$(git config github.username)" # used by git-go
+export GITHUB_USERNAME="$(git config github.username)" # used by git-go
 export GITHUB_URL="$(git config github.url)"    # used by gist
 
 export PROJECTS=~/Dev

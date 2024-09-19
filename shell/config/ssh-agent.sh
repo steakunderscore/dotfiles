7368b4511938836411ad424d29c48f5ab3c5de24
Henry Jenkins <henry@henryjenkins.name>
2024-09-19 11:19:06 +0100

    Make screen and ssh-agent work together
    https://superuser.com/questions/180148/how-do-you-get-screen-to-automatically-connect-to-the-current-ssh-agent-when-re

--- shell/config/ssh-agent.sh
+++ shell/config/ssh-agent.sh
@@ -1,4 +1,11 @@
-# This makes use of the systemd unit which sets up this socket
-if [ -e "/run/user/$(id -u)/ssh-agent.socket" ]; then
-  export SSH_AUTH_SOCK=/run/user/$(id -u)/ssh-agent.socket
+# Make use of the systemd unit which sets up this socket if agent's not already set
+if [ -z "${SSH_AUTH_SOCK}" ]; then
+  if [ -e "/run/user/$(id -u)/ssh-agent.socket" ]; then
+    export SSH_AUTH_SOCK=/run/user/$(id -u)/ssh-agent.socket
+  fi
+fi
+
+# Setup path for screen sessions to make use of ssh auth socket
+if [ -S "$SSH_AUTH_SOCK" ] && [ ! -h "$SSH_AUTH_SOCK" ]; then
+    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
